5455d1fc11da821c7b6015eae791f96dff385ad7
Rahamath22 <rahamath22104@gmail.com>
2024-03-07 12:03:51 +0530

    9 Question compketed

--- Conditional.java
+++ Conditional.java
@@ -5,7 +5,7 @@ public class Conditional
 {
     public static void main (String[] args)
     {
-       Q5();
+       Q9();
     }
 
     public static int GetInteger(String message)
@@ -101,10 +101,101 @@ public class Conditional
             System.out.println("You are not eligible to vote");
         }
     }
+    public static void Q7()
+    {
+        double Height=GetDouble("Enter your height in centimeter:");
+        if(Height>0 && Height<140)
+        {
+            System.out.println("You are short");
+        } 
+        else if(Height >140 && Height<160)
+        {
+            System.out.println("You are Medium");
+        }
+        else{
+            System.out.println("You are tall");
+        }
+    }
+    public static void Q8()
+    {
+        int FirstValue=GetInteger("Enter the first value");
+        int SecondValue=GetInteger("Enter the second value");
+        int ThirdValue=GetInteger("Enter the third value");
+        LargestThree(FirstValue, SecondValue, ThirdValue);
+    }
 
-
-
-
+    public static void LargestThree(int n1,int n2,int n3)
+    {
+        if(n1<n2)
+        {
+            if(n1>n2)
+            {
+                System.out.println("The First value is greatest: "+n2);
+            }
+            else{
+                System.out.println("The Second value is greatest: "+n1);
+            }
+        }
+        else if(n2<n3)
+        {
+            if(n2>n3)
+            {
+                System.out.println("The Second value is greatest: "+n2);
+            }
+            else{
+            System.out.println("The Third value is greatest: "+n3);
+            }
+        }
+        else{
+            if(n1>n3)
+            {
+                System.out.println("The First value is greatest: "+n1);
+            }
+            else{
+                System.out.println("The Third value is greatest: "+n3);
+            }
+        }
+    }
+    public static void Q9()
+    {
+        int FirstValue=GetInteger("Enter the first value");
+        int SecondValue=GetInteger("Enter the second value");
+        int ThirdValue=GetInteger("Enter the third value");
+        SmallestThree(FirstValue, SecondValue, ThirdValue);
+    }
+    public static void SmallestThree(int n1,int n2,int n3)
+    {
+        if(n1>n2)
+        {
+            if(n1<n2)
+            {
+                System.out.println("The First value is smallest: "+n1);
+            }
+            else{
+                System.out.println("The Second value is smallest: "+n2);
+            }
+        }
+        else if(n2>n3)
+        {
+            if(n2<n3)
+            {
+                System.out.println("The Second value is smallest: "+n2);
+            }
+            else{
+            System.out.println("The Third value is smallest: "+n3);
+            }
+        }
+        else{
+            if(n1<n3)
+            {
+                System.out.println("The First value is smallest: "+n1);
+            }
+            else{
+                System.out.println("The Third value is smallest: "+n3);
+            }
+        }
+    }
+    
 
     
 }
